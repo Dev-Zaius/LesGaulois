0db8d908e3d3436d0570189e45a56213d1bab8c4
paquet martin <pqm3914a@ens.fsi>
2022-09-26 19:47:48 +0200

    Panoramix

--- src/main/Test.java
+++ src/main/Test.java
@@ -1,6 +1,6 @@
 package main;
 import Politique.Village;
-
+import personnages.Druide;
 import personnages.Gaulois;
 import personnages.Romain;
 
@@ -12,11 +12,19 @@ public class Test {
 		Gaulois asterix = new Gaulois("Astérix",8);
 		Romain minus = new Romain("Minus",6);
 		
+		Druide panoramix = new Druide("Panoramix",5,10);
+		
 		asterix.parler("Bonjour à tous");
 		minus.parler("UN GAU... UN GAUGAU...");
 		asterix.frapper(minus);
 		asterix.frapper(minus);
 		minus.parler("J'abandonne...");
+		System.out.println("---------------------------------------");
+		panoramix.preparerPotion(10);
+		panoramix.preparerPotion(10);
+		panoramix.preparerPotion(10);
+		panoramix.preparerPotion(10);
+		panoramix.preparerPotion(10);
 	}
 
 }
