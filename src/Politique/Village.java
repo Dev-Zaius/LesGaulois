8dc6b09abe5250eb5df28ede77ee95bccc4b45b1
Dev-Zaius <martin.paquet@univ-tlse3.fr>
2022-10-05 14:40:43 +0200

    liste finie

--- src/Politique/Village.java
+++ src/Politique/Village.java
@@ -1,7 +1,45 @@
 package Politique;
 
+import java.util.Iterator;
+
+import personnages.Chef;
+import personnages.Gaulois;
+
 public class Village {
 	private String nom;
+	private Chef chef;
+	int nbVillageois = 0;
+	Gaulois[] villageois;
+	
+	public Village(String nom, int nbVillageoisMaximum) {
+		this.villageois = new Gaulois[nbVillageoisMaximum];
+		this.nom = nom;
+	}
+
+	public void ajouterHabitant(Gaulois gaulois) {
+		this.villageois[nbVillageois] = gaulois;
+		nbVillageois++;
+	}
+	
+	public Gaulois trouverHabitant(int numeroVillageois) {
+		return villageois[numeroVillageois];
+	}
+	
+	public void afficherVillageois() {
+		System.out.println("Dans village du chef " + this.chef.getNom()
+				         + " vivent les légendaires gaulois :");
+		
+		for (int i = 0 ; i < nbVillageois ; i++) {
+			System.out.println("- " + villageois[i].getNom());
+		}
+	}
+	
+	public void setChef(Chef chef) {
+		this.chef = chef;
+	}
+	
+
+
 	public String getNom() {
 		// TODO Auto-generated method stub
 		this.nom = nom;
--- src/main/Test.java
+++ src/main/Test.java
@@ -1,5 +1,6 @@
 package main;
 import Politique.Village;
+import personnages.Chef;
 import personnages.Druide;
 import personnages.Gaulois;
 import personnages.Romain;
@@ -9,11 +10,26 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Gaulois asterix = new Gaulois("Astérix",8);
-		Gaulois obélix = new Gaulois("Obélix",10);
+		Gaulois obélix = new Gaulois("Obélix",25);
 		Romain minus = new Romain("Minus",6);
+		Village village = new Village("Village des Irréductibles", 30);
+		Chef abraracourcix = new Chef("Abraracourcix",6,8,village);
+
+		
+		Gaulois gaulois = village.trouverHabitant(29);
+		// index out of bound car bornes dépassées
 		
 		Druide panoramix = new Druide("Panoramix",3,10);
 
+		village.ajouterHabitant(asterix);
+		village.ajouterHabitant(obélix);
+		village.setChef(abraracourcix);
+		village.afficherVillageois();
+		
+		System.out.println("--------------------------------------------");
+		//Gaulois mongaulois = village.trouverHabitant(1);
+		// Le gaulois mongaulois existe déjà il s'agit de asterix !
+		
 		panoramix.parler("Je vais aller préparer une petite potion...");
 		panoramix.preparerPotion(10);
 		panoramix.booster(obélix);
