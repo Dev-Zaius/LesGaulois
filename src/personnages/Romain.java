7b639a379f29a09f4ba89b5ddc3aa7e0e952463e
Dev-Zaius <martin.paquet@univ-tlse3.fr>
2022-10-05 14:55:30 +0200

    assert

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -5,6 +5,7 @@ public class Romain {
 	private int force;
 	
 	public Romain(String nom, int force) {
+		assert (force > 0) : "la force ne doit pas être négative";
 		this.nom = nom;
 		this.force = force;
 	}
@@ -18,11 +19,15 @@ public class Romain {
 		return "Le romain " + nom + " : ";
 	}
 	public void recevoirCoup(int forceCoup) {
+		assert (forceCoup> 0) : "la force du coup ne doit pas être négative";
+		int forceInitiale = this.force;
 		force -= forceCoup;
 		if (force > 0) {
 			parler("Aïe");
 		} else {
 			parler("J'abandonne...");
 		}
+		assert (forceInitiale > force) : "la force doit avoir diminuée";
+
 	}
 }
