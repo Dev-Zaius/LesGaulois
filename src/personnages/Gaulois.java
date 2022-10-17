f8280bff82dc894163461e7f59e926a4889526ce
Dev-Zaius <martin.paquet@univ-tlse3.fr>
2022-10-17 16:35:24 +0200

    C'est martin qui khalass

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -4,6 +4,8 @@ public class Gaulois {
 	private String nom;
 	private int force;
 	private int effetPotion = 1;
+	private int force, nb_trophees;
+	private Equipement trophees[] = new Equipement[100];
 	
 	public Gaulois(String nom, int force) {
 		super();
@@ -18,10 +20,21 @@ public class Gaulois {
 		System.out.println(prendreParole() + " « " + texte + " »");
 	}
 	
-	private String prendreParole() {
-		return "Le gaulois " + nom + " : ";
+//	private String prendreParole() {
+//		return "Le gaulois " + nom + " : ";
+//	}
+	public void frapper(Romain romain) {
+		System.out.println(nom + " envoie un grand coup dans la
+				mâchoire de " + romain.getNom());
+		Equipement trophees[] = romain.recevoirCoup((force / 3) *
+				effetPotion);
+		for (int i = 0; trophees != null && i < trophees.length; i++,
+		nb_trophees++) {
+			this.trophees[nb_trophees] = trophees[i];
+		}
+		return;
 	}
-	
+
 	public void frapper(Romain romain) {
 		System.out.println(nom + " envoie un grand coup dans la machoire de " + romain.getNom());
 		romain.recevoirCoup(force / 3);
